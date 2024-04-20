111bd36102cef0ab51ecd721aaf4be6634668b94
CSUG-Cole <146023803+CSUG-Cole@users.noreply.github.com>
2024-04-20 15:59:46 -0700

    Init CT repo with BankAccount class

---
+++ com/csc372/module_1/BankAccount.java
@@ -0,0 +1,12 @@
+package com.csc372.module_1;
+
+public class BankAccount {
+    private String firstName;
+    private String lastName;
+    private int accountID;
+    private double balance;
+
+    public BankAccount() {
+
+    }
+}
