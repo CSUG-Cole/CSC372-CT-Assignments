9ed8e7aadc6b571fc87789018c4b379a38abbdb9
CSUG-Cole <146023803+CSUG-Cole@users.noreply.github.com>
2024-05-12 12:21:59 -0700

    Add Sphere class

---
+++ com/csc372/module_4/Sphere.java
@@ -0,0 +1,23 @@
+package com.csc372.module_4;
+
+public class Sphere extends Shape
+{
+    private double radius;
+
+    /* Constructor */
+    public Sphere(double radius) { this.radius = radius; }
+
+    /* Override base class methods */
+    /* Formula 4πr^2 */
+    @Override
+    public double surfaceArea() { return 4 * Math.PI * radius * radius; }
+
+    /* Formula is 4/3πr^3 */
+    @Override
+    public double volume() { return (4.0 / 3) * Math.PI * radius * radius * radius; }
+
+    @Override
+    public String toString() {
+        return "Sphere: SurfaceArea = " + surfaceArea() + ", Volume = " + volume();
+    }
+}
