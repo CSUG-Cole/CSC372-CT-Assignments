9920a32b7dc85cf461f3eb56166d7bd79becdc82
CSUG-Cole <146023803+CSUG-Cole@users.noreply.github.com>
2024-05-12 12:24:55 -0700

    Add the Cylinder class

---
+++ com/csc372/module_4/Cylinder.java
@@ -0,0 +1,27 @@
+package com.csc372.module_4;
+
+public class Cylinder extends Shape
+{
+    private double radius;
+    private double height;
+
+    /* Constructor */
+    public Cylinder(double radius, double height) {
+        this.radius = radius;
+        this.height = height;
+    }
+
+    /* Override base class methods */
+    /* Formula 2πr(r+h) */
+    @Override
+    public double surfaceArea() { return 2 * Math.PI * radius * (radius + height); }
+
+    /* Formula is πhr^2 */
+    @Override
+    public double volume() { return Math.PI * height * radius * radius; }
+
+    @Override
+    public String toString() {
+        return "Cylinder: SurfaceArea = " + surfaceArea() + ", Volume = " + volume();
+    }
+}
